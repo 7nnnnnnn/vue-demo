19cd0191a07f4060726516cf9032438704f9890e
ync22 <12882227+ync22@user.noreply.gitee.com>
2023-06-13 15:00:37 +0800

    test 2

---
+++ assets/userinfo-4a737bab.js
@@ -0,0 +1,1 @@
+import{i as o}from"./index-1c8d0cbc.js";const s=e=>o.request({url:"api/mobile/elogin",method:"post",data:e,headers:{}});export{s as u};
--- assets/userinfo-75373bb8.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./index-db1a1148.js";const s=e=>o.request({url:"api/mobile/elogin",method:"post",data:e,headers:{}});export{s as u};
